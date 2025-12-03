ec653a84a31b70820067df7796b0637694c6a582
Jacek Kotlarski <jacek.kotlarski@bioseco.com>
2025-12-03 14:13:59 +0100

    feat: implement ObsGraphConfigurator with configuration management and add keys for API integration

---
+++ obsgraph_flask/lib/keys.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+keys.py
+Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+Created: 3.12.2025, 08:55:04
+
+Purpose:
+"""
+
+from jsktoolbox.attribtool import ReadOnlyClass
+
+
+class ObsKeys(object, metaclass=ReadOnlyClass):
+    """
+    ObsGraph Flask application keys
+    """
+
+    # Application secret key
+    CONF_MAIN_SECTION_NAME: str = "ObsGraphFlaskApp"
+    CONF_SALT: str = "salt"
+    CONF_OBSERVIUM_API_URL: str = "observium_api_url"
+    CONF_API_LOGIN: str = "api_login"
+    CONF_API_PASSWORD: str = "api_password"
+
+
+# #[EOF]#######################################################################
--- obsgraph_flask/tools/obsgraph_configurator.py
+++ obsgraph_flask/tools/obsgraph_configurator.py
@@ -9,19 +9,92 @@ Purpose: ObsGraph configuration tool
 License: MIT
 """
 
+from inspect import currentframe
 import os
 import sys
 
 from jsktoolbox.configtool import Config
+from jsktoolbox.basetool import BData
+from jsktoolbox.raisetool import Raise
+from jsktoolbox.attribtool import ReadOnlyClass
 
-# current dynamic project directory
-current_dir: str = os.path.dirname(os.path.abspath(__file__))
-config_file_path: str = os.path.join(current_dir, "obsgraph.conf")
+from ..lib.keys import ObsKeys
+
+
+class _Keys(object, metaclass=ReadOnlyClass):
+    """Configuration keys for ObsGraphConfigurator."""
+
+    CONF: str = "__config__"
+
+
+class ObsGraphConfigurator(BData):
+    """ObsGraph configuration tool."""
+
+    def __init__(self) -> None:
+        """Initialize the configurator with the given config file path."""
+        # current dynamic project directory
+        current_dir: str = os.path.dirname(os.path.abspath(__file__))
+        config_file_path: str = os.path.join(current_dir, "obsgraph.conf")
+
+        self._set_data(
+            key=_Keys.CONF,
+            value=Config(
+                filename=config_file_path,
+                main_section_name=ObsKeys.CONF_MAIN_SECTION_NAME,
+                auto_create=True,
+            ),
+            set_default_type=Config,
+        )
+
+        if not self.__config.file_exists:
+            self.__create_config()
+
+    def __create_config(self) -> None:
+        """Create a default configuration file if it does not exist."""
+        conf: Config = self.__config
+
+        if not conf.file_exists:
+            conf.set(
+                section=ObsKeys.CONF_MAIN_SECTION_NAME,
+                varname=ObsKeys.CONF_SALT,
+                value=0,
+            )
+            conf.set(
+                section=ObsKeys.CONF_MAIN_SECTION_NAME,
+                varname=ObsKeys.CONF_OBSERVIUM_API_URL,
+                value="",
+            )
+            conf.set(
+                section=ObsKeys.CONF_MAIN_SECTION_NAME,
+                varname=ObsKeys.CONF_API_LOGIN,
+                value="",
+            )
+            conf.set(
+                section=ObsKeys.CONF_MAIN_SECTION_NAME,
+                varname=ObsKeys.CONF_API_PASSWORD,
+                value="",
+            )
+            conf.save()
+
+    @property
+    def __config(self) -> Config:
+        """Get the configuration object."""
+        conf = self._get_data(key=_Keys.CONF)
+        if not conf or not isinstance(conf, Config):
+            raise Raise.error(
+                f"Configuration not set or invalid type: {type(conf)}",
+                RuntimeError,
+                self._c_name,
+                currentframe(),
+            )
+
+        return conf
+
 
 if __name__ == "__main__":
     print("ObsGraph Configurator")
     print("======================\n")
-    print(f"Loading configuration file: {config_file_path}\n")
+    configurator = ObsGraphConfigurator()
 
     sys.exit(0)
 
